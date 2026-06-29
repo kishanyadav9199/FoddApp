315ea1cf5214b86d3c43042b1ce496de568a1504
kishanyadav9199 <wwwkishan763@gmail.com>
2026-06-29 16:32:53 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,43 +3,46 @@ import './Footer.css'
 import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
-    <div className='footer' id='footer'>
+    <div className="footer" id="footer">
       <div className="footer-content">
-      
         <div className="footer-content-left">
-        <img src={assets.logo} alt="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptatibus quis possimus esse rem suscipit dicta nam nihil architecto aliquid? Praesentium expedita excepturi, voluptatem incidunt architecto repudiandae eveniet optio aut.</p>        
-        <div className="footer-social-icons">
-           <img src={assets.facebook_icon} alt="" /> 
-           <img src={assets.twitter_icon} alt="" /> 
-           <img src={assets.linkedin_icon} alt="" /> 
-        </div>
+          <img src={assets.logo} alt="" />
+          <p>
+            Explore a wide range of delicious meals, freshly prepared and
+            delivered to your doorstep. Order your favorite food quickly and
+            enjoy a seamless online food ordering experience..
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
 
         <div className="footer-content-centre">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privicy policy</li>
-            </ul>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privicy policy</li>
+          </ul>
         </div>
-
 
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+9192937467813</li>
-                <li>www@gmail.com</li>
-            </ul>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+919199970983</li>
+            <li>www763@gmail.com</li>
+          </ul>
         </div>
-        
       </div>
       <hr />
-      <p className='footer-copyright'>Copyright 2024 @ Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com - All Right Reserved.
+      </p>
     </div>
-  )
+  );
 }
 
 export default Footer
